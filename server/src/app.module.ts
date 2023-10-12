b1aedc7c0915e94af7fe5d7399b36a8ec91e33ea
winfixx <winfixxx@gmail.com>
2023-10-12 19:27:14 +0600

    удалил ненужные директории

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { AppMiddleware } from './middleware/app.middleware'
 
 @Module({
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule implements NestModule {
-    configure(consumer: MiddlewareConsumer) {
-        consumer
-            .apply(AppMiddleware)
-            .forRoutes()
-    
-    }
+export class AppModule  {
+
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function start() {
-    const PORT = process.env.PORT || 6000
+    const PORT = process.env.PORT || 4000
     
     try {
         const app = await NestFactory.create(AppModule)
