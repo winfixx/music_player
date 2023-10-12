23aa112c9750538637d208a1bc04a9a17ce9fc05
winfixx <winfixxx@gmail.com>
2023-10-12 20:39:29 +0600

    удалил юзер модуль

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { UserModule } from './user/user.module'
 const test2 = 'test2'
 @Module({
-    imports: [UserModule],
     controllers: [AppController],
     providers: [AppService],
 })
